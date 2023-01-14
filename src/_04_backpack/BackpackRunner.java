043588daa09965171a119e20ebcc914a417b11a1
League <kierancronin213@gmail.com>
2023-01-13 18:24:37 -0800

    Backpack

--- src/_04_backpack/BackpackRunner.java
+++ src/_04_backpack/BackpackRunner.java
@@ -2,6 +2,18 @@ package _04_backpack;
 
 public class BackpackRunner {
 	public static void main(String[] args) {
-		new Backpack().packAndCheck();
+		
+		Backpack backpack = new Backpack();
+		Pencil pencil = new Pencil();
+		Ruler ruler = new Ruler();
+		Textbook textbook = new Textbook();
+		
+		backpack.putInBackpack(pencil);
+		pencil.write("Answer: Biggie Cheese was a stronk and powerful boi. He had 3 biceps and 1 foot, yet he still conquered Europe. His favorite snack was baguette.");
+		backpack.putInBackpack(ruler);
+		ruler.measure();
+		backpack.putInBackpack(textbook); 
+		textbook.read();
+		backpack.packAndCheck();
 	}
 }
